d5e474e8901f354715899893b8e4e6a1981b4911
Invenio <info@inveniosoftware.org>
2018-03-16 15:40:14 +0100

    global: license change to MIT License

--- dcxml/__init__.py
+++ dcxml/__init__.py
@@ -1,26 +1,10 @@
 # -*- coding: utf-8 -*-
 #
 # This file is part of dcxml.
-# Copyright (C) 2016 CERN.
+# Copyright (C) 2016-2018 CERN.
 #
-# dcxml is free software; you can redistribute it
-# and/or modify it under the terms of the GNU General Public License as
-# published by the Free Software Foundation; either version 2 of the
-# License, or (at your option) any later version.
-#
-# dcxml is distributed in the hope that it will be
-# useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with dcxml; if not, write to the
-# Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
-# MA 02111-1307, USA.
-#
-# In applying this license, CERN does not
-# waive the privileges and immunities granted to it by virtue of its status
-# as an Intergovernmental Organization or submit itself to any jurisdiction.
+# dcxml is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
 
 """Dublin Core XML generation from Python dictionaries.
 
@@ -44,7 +28,7 @@ First let's create a Python dictionary with all 15 elements:
 ...     languages = ['en'],
 ...     publishers = ['CERN'],
 ...     relations = ['Invenio Software'],
-...     rights = ['GPLv2'],
+...     rights = ['MIT'],
 ...     sources = ['Python'],
 ...     subject = ['XML'],
 ...     titles = ['Dublin Core XML'],
@@ -76,7 +60,7 @@ and print the 15 elements (without the container element)
   <dc:language>en</dc:language>
   <dc:publisher>CERN</dc:publisher>
   <dc:relation>Invenio Software</dc:relation>
-  <dc:rights>GPLv2</dc:rights>
+  <dc:rights>MIT</dc:rights>
   <dc:source>Python</dc:source>
   <dc:title>Dublin Core XML</dc:title>
   <dc:type>Software</dc:type>
